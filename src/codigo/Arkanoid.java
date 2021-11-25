7f59c7da7a9f24fbb53ae50220314133b1720d60
quiiques <quiique00@gmail.com>
2021-11-25 15:49:02 +0100

    iniciado el fondo del marcador TODO: añadir fuentes arkanoid, vidas, mejorar rebote

--- src/codigo/Arkanoid.java
+++ src/codigo/Arkanoid.java
@@ -3,6 +3,7 @@ import java.awt.Color;
 import java.awt.event.MouseEvent;
 
 import acm.graphics.GImage;
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 public class Arkanoid extends GraphicsProgram {
 
@@ -10,16 +11,21 @@ public class Arkanoid extends GraphicsProgram {
 	static final int ALTO_LADRILLO =15;
 	static final int ANCHO_PANTALLA = 522;
 	static final int ALTO_PANTALLA =586;
+	static final int ANCHO_MARCADOR=300;
 	Bola bola1 = new Bola(10,10,Color.BLUE);
 	//declaro el cursor del juego
-	Cursor miCursor = new Cursor(0,400,60,10,Color.GREEN);
+	Cursor miCursor = new Cursor(0,400,60,10,Color.RED);
 	//declaro el fondo del juego
 	GImage fondo = new GImage("imagenes/fondo500,500.png");
+	GRect fondoMarcador = new GRect(ANCHO_MARCADOR,ALTO_PANTALLA);
 
 	public void init(){
+		//añado el fondo del marcador
+		fondoMarcador.setFilled(true);
+		add(fondoMarcador,ANCHO_PANTALLA-22,0);
 		//añado dimensiones de la pantalla
-		setSize(ANCHO_PANTALLA,ALTO_PANTALLA);
-		//añado fondo lo primero porque asi se queda al fondo
+		setSize(ANCHO_PANTALLA+ANCHO_MARCADOR,ALTO_PANTALLA);
+		//añado el fondo lo primero porque asi se queda al fondo
 		add(fondo);
 		addMouseListeners();
 		add(bola1,50,100);	
